7acf9b71be82bdb27b970519c2b7bc01d4d178db
Carlos Kazuo Mochizuki Ishizaka <carlos.ishizaka@dafiti.com.br>
2020-02-05 16:19:58 -0300

    feat: 🎸 done chapter 3

--- packages/graphql-node/src/index.js
+++ packages/graphql-node/src/index.js
@@ -1,22 +1,61 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-// 1
-const typeDefs = `
-  type Query {
-    info: String!
-  }
-`
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}]
 
-// 2
+let idCount = links.length
 const resolvers = {
   Query: {
-    info: () => 'This is the API of a Hackernews Clone'
+    feed: () => links,
+    link: (parent, args) => links.find(({ id }) => id === args.id)
+  },
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url
+      }
+
+      links.push(link)
+
+      return link
+    },
+    updateLink: (parent, args) => {
+      const link = links.find(({ id }) => id === args.id)
+
+      if (!link) {
+        return null
+      }
+
+      link.url = args.url
+      link.description = args.description
+
+      return link
+    },
+    deleteLink: (parent, args) => {
+      const link = links.find(({ id }) => id === args.id)
+
+      if (!link) {
+        return null
+      }
+
+      return links.splice(links.indexOf(link), 1).pop()
+    }
   }
+  // Link: {
+  //   id: (parent) => parent.id,
+  //   description: (parent) => parent.description,
+  //   url: (parent) => parent.url
+  // }
+  
 }
 
-// 3
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers
 })
 
