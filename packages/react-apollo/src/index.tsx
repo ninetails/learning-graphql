c7fa5d2d894da4fd1ba0527385231cd69ddd1452
Carlos Kazuo Mochizuki Ishizaka <carlos.ishizaka@dafiti.com.br>
2020-02-07 18:10:26 -0300

    feat: 🎸 halt on error

--- packages/react-apollo/src/index.tsx
+++ packages/react-apollo/src/index.tsx
@@ -1,16 +1,33 @@
 import React from 'react'
 import { createRoot } from 'react-dom'
 import invariant from 'tiny-invariant'
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 import './index.css'
 import App from './components/App'
 import * as serviceWorker from './serviceWorker'
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+})
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
 
 const rootEl = document.getElementById('root') ?? null
 invariant(rootEl !== null, 'Root element not found')
 const root = createRoot(rootEl, { hydrate: rootEl.hasChildNodes() })
 const render = root.render.bind(root)
 
-render(<App />)
+render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
